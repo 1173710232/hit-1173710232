834ca86b5b823692a95e3901ea0bdec83b1d9091
1173710220 <47965311+1173710220@users.noreply.github.com>
2019-07-07 20:57:36 +0800

    Update Hello.java

--- hit-1173710232/src/hello/Hello.java
+++ hit-1173710232/src/hello/Hello.java
@@ -3,6 +3,6 @@ package hello;
 public class Hello {
   public static void main(String args[]) {
     System.out.println("Hello World!");
-    SYStem.out.println("GY is a clever girl !");
+    SYStem.out.println("GY is a very clever girl !");
   }
 }
